b7968280b0b0a4efb143fa7350c39065dc7ddc57
Luiz Cezer <lccezinha@gmail.com>
2015-06-23 11:46:43 -0300

    Plataformas

--- js/scripts.js
+++ js/scripts.js
@@ -14,15 +14,4 @@ $(function() {
     $(this).addClass('active');
     e.preventDefault();
   });
-
-  //Fix
-  $("#new-comment").click(function(){
-    var comment = $('#comment').val()
-
-    if(comment == ''){
-      $("#alert-message").css('display', 'block');
-    }else{
-      location.href = "/jogo.html"
-    }
-  })
 });
